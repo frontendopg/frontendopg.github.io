6a4c285fd00782f5031d8f3de2d363a24c63e8ed
DaniilB2 <daniil.bazilevsky@gmail.com>
2023-12-07 09:29:28 +0300

    deplo

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,11 @@ git init
 git add -A
 git add .
 git commit -m 'deploy'
-
+git checkout master
+git branch -m main
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:frontendopg/frontendopg.github.io.git main
+ git push -f https://github.com/frontendopg/frontendopg.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
