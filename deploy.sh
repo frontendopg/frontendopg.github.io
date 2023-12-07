2bf80840723fc8c75ed5222fced9a19fb84702a9
DaniilB2 <daniil.bazilevsky@gmail.com>
2023-12-07 09:13:13 +0300

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
 npm run build
+
 # navigate into the build output directory
 cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
+git add .
 git commit -m 'deploy'
-git push -f git@github.com:frontendopg/frontendopg.github.io.git
+
+
+# if you are deploying to https://<USERNAME>.github.io
+ git push -f git@github.com:frontendopg/frontendopg.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
 cd -
