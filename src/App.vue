d9dd69e34d1fed6c05d40b2feeb10e4c9c27c2a6
Alex <keks63620@gmail.com>
2023-12-09 21:42:05 +0300

    Create Competensies
    I created Competensies

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CompetensiesComponent></CompetensiesComponent>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+import CompetensiesComponent from '@/components/Competensies.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CompetensiesComponent
   }
 }
 </script>
